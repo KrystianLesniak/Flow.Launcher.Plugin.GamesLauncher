e1c2c235d70040b07fcc1dbfae81861c6c07bd3c
krystian.lesniak <kontakt@krystianlesniak.pl>
2023-08-21 22:13:58 +0200

    Updated release.ps1

--- release.ps1
+++ release.ps1
@@ -1,2 +1,2 @@
 dotnet publish Flow.Launcher.Plugin.GamesLauncher -c Release -r win-x64 --no-self-contained
-Compress-Archive -LiteralPath Flow.Launcher.Plugin.GamesLauncher/bin/Release/win-x64/publish -DestinationPath Flow.Launcher.Plugin.GamesLauncher/bin/GamesLauncher.zip -Force
+Compress-Archive -Path Flow.Launcher.Plugin.GamesLauncher/bin/Release/win-x64/publish/* -DestinationPath Flow.Launcher.Plugin.GamesLauncher/bin/GamesLauncher.zip -Force
